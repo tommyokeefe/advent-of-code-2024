82f3b66a65cb887a330fd4d5e371a858c589ef44
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-01 21:00:16 +0000

    fix eslint to work with vitest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,18 @@ module.exports = {
   env: {
     es2022: true,
     node: true,
+    'vitest-globals/env': true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:vitest-globals/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   plugins: [],
   rules: {
     'no-fallthrough': 'off',
     'no-unused-vars': 'off',
     'prefer-const': 'warn',
-  },
-  //sourceType: 'module',
+  }
 }
