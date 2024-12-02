dfc8fceda4a1240b1b481bd6bcac77ab59e4bf0c
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-02 12:54:45 +0000

    day 2 part 1

--- src/day-02/index.js
+++ src/day-02/index.js
@@ -1,9 +1,55 @@
-import { getInput } from '../utils'
+import { getInput, splitOnWhiteSpace } from '../utils'
 
 const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+const ASCENDING = 'asc'
+const DESCENDING = 'desc'
+
+export function solution1(input) {
+    const reports = formatInput(input)
+    const total = reports.reduce((safeReports, currentReport) => {
+        let direction
+        const levels = splitOnWhiteSpace(currentReport).map(item => parseInt(item))
+        const first = levels[0]
+        const second = levels[1]
+
+        if (first === second) {
+            return safeReports
+        }
+
+        direction = first < second ? ASCENDING : DESCENDING
+
+        const isSafe = levels.reduce((safe, currentLevel, index, levelsArray) => {
+            if (!safe) {
+                levelsArray.splice(1); // break out of the reducer
+                return safe
+            }
+
+            if (index + 1 === levelsArray.length) {
+                return safe
+            }
+
+            const nextLevel = levelsArray[index + 1]
+
+            if (direction === ASCENDING) {
+                return (currentLevel < nextLevel && currentLevel >= nextLevel - 3)
+            }
+
+            return (currentLevel > nextLevel && currentLevel <= nextLevel + 3)
+        }, true)
+
+        if (isSafe) {
+            return safeReports + 1
+        }
+
+        return safeReports
+    }, 0)
+
+    return total
+}
 
 export function solution2(input) {}
+
+// console.log(solution1(data)); // 631
--- src/day-02/index.test.js
+++ src/day-02/index.test.js
@@ -1,9 +1,14 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(2)
 })
 
 test('solution2', () => {
