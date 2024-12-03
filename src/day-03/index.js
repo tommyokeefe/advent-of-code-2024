469717e8905973e80d8efa66fe6a517e9d9cd7a0
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-03 12:16:38 +0000

    day 3 part 2 - fun with regex and reducers

--- src/day-03/index.js
+++ src/day-03/index.js
@@ -2,18 +2,54 @@ import { getInput } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
-
 export function solution1(input) {
-    let total = 0;
-    const validInstructions = input.matchAll(/mul\((\d+),(\d+)\)/g)
-    for (const step of validInstructions) {
-        const [_, value1, value2] = step
-        total = total + value1 * value2
-    }
-    return total
+  let total = 0
+  const validInstructions = input.matchAll(/mul\((\d+),(\d+)\)/g)
+  for (const step of validInstructions) {
+    const [_, value1, value2] = step
+    total = total + value1 * value2
+  }
+  return total
 }
 
-export function solution2(input) {}
+export function solution2(input) {
+  const instructions = []
+  let shouldDo = true
+
+  const validInstructions = input.matchAll(/mul\((\d+),(\d+)\)/g)
+  for (const step of validInstructions) {
+    const [_, value1, value2] = step
+    instructions.push({ type: 'mul', index: step.index, value1, value2 })
+  }
+
+  const doInstructions = input.matchAll(/do\(\)/g)
+  for (const step of doInstructions) {
+    instructions.push({ type: 'do', index: step.index })
+  }
+
+  const doNotInstructions = input.matchAll(/don't\(\)/g)
+  for (const step of doNotInstructions) {
+    instructions.push({ type: 'dont', index: step.index })
+  }
+
+  const sortedInstructions = instructions.toSorted((a, b) => a.index - b.index)
+  return sortedInstructions.reduce((total, step) => {
+    const { type, value1, value2 } = step
+    switch (type) {
+      case 'mul':
+        if (shouldDo) {
+          return total + value1 * value2
+        }
+        return total
+      case 'do':
+        shouldDo = true
+        return total
+      case 'dont':
+        shouldDo = false
+        return total
+    }
+  }, 0)
+}
 
 // console.log(solution1(data)) // 188116424
+// console.log(solution2(data)) // 104245808
--- src/day-03/index.test.js
+++ src/day-03/index.test.js
@@ -1,11 +1,11 @@
 import { solution1, solution2 } from './'
 
-const input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 test('solution1', () => {
   expect(solution1(input)).toEqual(161)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(48)
 })
