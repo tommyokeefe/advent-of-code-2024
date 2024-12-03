109225a23e401741663a51330b69470497607a17
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-03 11:51:54 +0000

    Day 3 part one

--- src/day-03/index.js
+++ src/day-03/index.js
@@ -4,6 +4,16 @@ const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+export function solution1(input) {
+    let total = 0;
+    const validInstructions = input.matchAll(/mul\((\d+),(\d+)\)/g)
+    for (const step of validInstructions) {
+        const [_, value1, value2] = step
+        total = total + value1 * value2
+    }
+    return total
+}
 
 export function solution2(input) {}
+
+// console.log(solution1(data)) // 188116424
--- src/day-03/index.test.js
+++ src/day-03/index.test.js
@@ -1,9 +1,9 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(161)
 })
 
 test('solution2', () => {
