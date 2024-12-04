59f2497cc66e6126113661ec3a022117fa4dca4b
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-04 14:11:53 +0000

    Day4 WIP - bad results :(

--- src/day-04/index.js
+++ src/day-04/index.js
@@ -4,6 +4,89 @@ const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+const isChar = (char) => (input) => char === input
+const isX = isChar('X')
+const isM = isChar('M')
+const isA = isChar('A')
+const isS = isChar('S')
+
+const directionList = [
+    { x: -1, y: 0 },
+    { x: -1, y: -1 },
+    { x: 0, y: -1 },
+    { x: 1, y: -1 },
+    { x: 1, y: 0 },
+    { x:1, y: 1 },
+    { x: 0, y: 1 },
+    { x: -1, y: 1 }
+]
+
+const createGameBoard = (lines) => {
+    const gameBoard = []
+    lines.forEach((line, lineIndex) => {
+        line.trim().split('').forEach((character, charIndex) => {
+            gameBoard.push({
+                value: character,
+                position: { x: charIndex, y: lineIndex },
+                key: `${charIndex}${lineIndex}`
+            })
+        })
+    })
+    return gameBoard
+}
+
+const getNewPosition = (position, direction) => ({
+    x: position.x + direction.x,
+    y: position.y + direction.y,
+})
+
+const createNavigateAndCheckValue = (gameBoard) => (position, direction, valid) => {
+    const newPosition = getNewPosition(position, direction)
+    const key = `${newPosition.x}${newPosition.y}`
+    const char = gameBoard.find(character => character.key === key)
+
+    if (!char || !valid(char.value)) {
+        return undefined
+    }
+    return newPosition
+}
+
+export function solution1(input) {
+    const lines = formatInput(input)
+    const gameBoard = createGameBoard(lines)
+    const navigateAndCheckValue = createNavigateAndCheckValue(gameBoard)
+
+    const result = gameBoard.reduce((total, character) => {
+        const { value, position } = character
+        if (!isX(value)) {
+            return total
+        }
+
+        return directionList.reduce((currentTotal, direction) => {
+            // check m
+            let newPosition = navigateAndCheckValue(position, direction, isM);
+            if (!newPosition) {
+                return currentTotal
+            }
+
+            // check a
+            newPosition = navigateAndCheckValue(newPosition, direction, isA);
+            if (!newPosition) {
+                return currentTotal
+            }
+
+            // check s
+            newPosition = navigateAndCheckValue(newPosition, direction, isS);
+            if (!newPosition) {
+                return currentTotal
+            }
+
+            return currentTotal + 1
+        }, total)
+    }, 0)
+    return result
+}
 
 export function solution2(input) {}
+
+console.log(solution3(data)) // 2196 (wrong)
--- src/day-04/index.test.js
+++ src/day-04/index.test.js
@@ -1,9 +1,31 @@
+import { expect } from 'vitest'
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
+const inputAlt = `MMMMMMMMMM
+MSMMSMMSMM
+MMAMAMAMMM
+MMMMMMMMMM
+MSAMXMASMM
+MMMMMMMMMM
+MMAMAMAMMM
+MSMMSMMSMM
+MMMMMMSAMX`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(18) //2344
+
+  expect(solution1(inputAlt)).toEqual(9)
 })
 
 test('solution2', () => {
