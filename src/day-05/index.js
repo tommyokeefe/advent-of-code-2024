2b1d0ad920c87aa94e89cbfe7c2ec2d2185238d0
Tommy O'Keefe <tommyokeefe81@gmail.com>
2024-12-05 14:26:08 +0000

    Day 5 WIP - part 2 still failing

--- src/day-05/index.js
+++ src/day-05/index.js
@@ -2,8 +2,86 @@ import { getInput } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input => {
+  const [rawInstructions, rawUpdates] = input.trim().split('\n\n')
+  const instructions = rawInstructions
+    .trim()
+    .split('\n')
+    .map(instruction => {
+      const [left, right] = instruction.split('|')
+      return { left: parseInt(left), right: parseInt(right) }
+    })
+  const updates = rawUpdates
+    .trim()
+    .split('\n')
+    .map(update => update.split(',').map(Number))
+  return [instructions, updates]
+}
 
-export function solution1(input) {}
+const createUpdateFilter = (instructions, isGood) => update => {
+  let good = isGood
+  update.forEach((item, index) => {
+    const validInstructions = instructions.filter(
+      instruction => instruction.left === item
+    )
+    validInstructions.forEach(instruction => {
+      const rightIndex = update.findIndex(value => value === instruction.right)
+      if (rightIndex !== -1 && rightIndex < index) {
+        good = !isGood
+      }
+    })
+  })
+  return good
+}
 
-export function solution2(input) {}
+const sortUpdate = (update, instructions) => {
+  let changes = false
+  let tempUpdate = update
+  update.forEach(item => {
+    const leftIndex = tempUpdate.findIndex(value => value === item)
+    const indexes = instructions
+      .filter(instruction => instruction.left === item)
+      .map(instruction =>
+        tempUpdate.findIndex(value => value === instruction.right)
+      )
+      .filter(instruction => instruction >= 0)
+      .sort()
+    const lowestIndex = indexes[0]
+    if (leftIndex > lowestIndex) {
+      changes = true
+      const leftValue = tempUpdate[leftIndex]
+      tempUpdate = tempUpdate
+        .toSpliced(leftIndex, 1)
+        .toSpliced(lowestIndex, 0, leftValue)
+    }
+  })
+
+  if (changes) {
+    return sortUpdate(tempUpdate, instructions)
+  }
+
+  return tempUpdate
+}
+
+const calculateMiddleReducer = (total, update) => {
+  const middleIndex = (update.length - 1) / 2
+  return update[middleIndex] + total
+}
+
+export function solution1(input) {
+  const [instructions, updates] = formatInput(input)
+  const goodUpdates = updates.filter(createUpdateFilter(instructions, true))
+  return goodUpdates.reduce(calculateMiddleReducer, 0)
+}
+
+export function solution2(input) {
+  const [instructions, updates] = formatInput(input)
+  const badUpdates = updates.filter(createUpdateFilter(instructions, false))
+  const sortedUpdates = badUpdates.map(update => {
+    return sortUpdate(update, instructions)
+  })
+  return sortedUpdates.reduce(calculateMiddleReducer, 0)
+}
+
+// console.log(solution1(data)) // 7074
+console.log(solution2(data))
--- src/day-05/index.test.js
+++ src/day-05/index.test.js
@@ -1,11 +1,39 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(143)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(123)
 })
